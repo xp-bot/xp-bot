64933d2c935bed539c6a967524dc21103d0e8611
ClearMist <cringleysdays@outlook.com>
2023-10-29 22:03:03 +0000

    feat: send user who invited XP info

--- src/handlers/discord-events/index.ts
+++ src/handlers/discord-events/index.ts
@@ -2,9 +2,11 @@ import { Client } from 'discord.js';
 import discordClient from '../../clients/discord-client';
 import interactionCreate from './interaction-create';
 import ready from './ready';
+import guildCreate from './guild-create';
 
 export default (client?: Client) => {
   const c = client || discordClient;
   ready(c);
   interactionCreate(c);
+  guildCreate(c);
 };
