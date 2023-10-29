fc4a3db36fc873d4c0a3129e2d374215e596f127
ConnysCode <concode@outlook.de>
2023-10-29 21:36:46 +0100

    feat(typing): add env types

---
+++ src/types/global.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    TOKEN: string;
+    APPLICATION_ID: string;
+    DEBUG_GUILD_ID: string;
+    API: string;
+    API_TOKEN: string;
+    ILUM_API: string;
+    ILUM_API_TOKEN: string;
+  }
+}
