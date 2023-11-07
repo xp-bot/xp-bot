8a86ebe0822304a23e29c7545ed88332f08b758a
ConnysCode <concode@outlook.de>
2023-11-07 16:39:51 +0100

    feat(rank): add incognito check

--- src/commands/info/rank.ts
+++ src/commands/info/rank.ts
@@ -1,17 +1,42 @@
+import { GuildMemberService } from '../../api/generated';
 import Command from '../../classes/command';
 import XPError, { XPErrorType } from '../../classes/xp-error';
+import defaultEmbed, {
+  DefaultEmbedType,
+} from '../../helpers/messaging/default-embed';
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { t } from 'i18next';
 
 const execute = async (interaction: ChatInputCommandInteraction) => {
+  const guildMemberService_ = GuildMemberService;
+
   const userId =
     interaction.options.getUser('user', false)?.id || interaction?.user?.id;
   const guildId = interaction?.guildId;
-  if (guildId) throw new XPError(XPErrorType.INTERACTION_GUILD_UNRESOLVABLE);
+  if (!guildId) throw new XPError(XPErrorType.INTERACTION_GUILD_UNRESOLVABLE);
   if (!userId) throw new XPError(XPErrorType.INTERACTION_USER_UNRESOLVABLE);
 
+  const guildMember = await guildMemberService_.getGuildMember({
+    guildId,
+    userId,
+  });
+
+  if (guildMember.settings.incognito) {
+    const embed = defaultEmbed(DefaultEmbedType.INFO);
+    embed
+      .setTitle('Incognito')
+      .setDescription(
+        interaction.user.id === userId
+          ? 'You are currently in incognito mode. Your ranking card is hidden.'
+          : 'This user is currently in incognito mode. Their ranking card is hidden.',
+      );
+
+    interaction.reply({ embeds: [embed] });
+    return;
+  }
+
   interaction.reply(
-    `https://bot-api.xp-bot.net/rank/${guildId}/${userId}?${Date.now()}`,
+    `${process.env.API}rank/${guildId}/${userId}?${Date.now()}`,
   );
 };
 
